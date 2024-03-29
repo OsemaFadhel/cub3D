b91d740dcb9bf3aa1b1c6003f31f69d640b5e90f
Osema Fadhel <ofadhel@e4r13p1.42roma.it>
2024-03-29 16:07:24 +0100

    cub3d

--- parser/check_closed.c
+++ parser/check_closed.c
@@ -6,7 +6,7 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 16:04:46 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/03/29 02:28:27 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/03/29 16:07:15 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,31 +61,52 @@ void	check_closed_edges(t_game *game)
 		if (game->map.map[y][0] == '1' || game->map.map[y][0] == ' ')
 			;
 		else if (game->map.map[y][0] == ' ')
+		{
 			check_space_left(game, y, 1);
+			check_space_left(game, y + 1, 0);
+			check_space_left(game, y - 1, 0);
+		}
 		else
 			ft_exit(game, 3);
 		if (game->map.map[y][x] == '1' || game->map.map[y][x] == ' ')
 			;
 		else if (game->map.map[y][x] == ' ')
+		{
 			check_space_right(game, y, x - 1);
+			check_space_right(game, y + 1, x);
+			check_space_right(game, y - 1, x);
+		}
 		else
 			ft_exit(game, 3);
 		y++;
 	}
 }
 
-void	check_space_bottom(t_game *game, int y, int x)
+void	check_space_bottom_left(t_game *game, int y, int x)
 {
-	printf("space map[%d][%d] = %c\n", y, x, game->map.map[y][x]);
 	if (x < 0 || y < 0 || y >= game->map.height || x >= game->map.width)
 		return ;
 	else if (game->map.map[y][x] == '1')
 		return ;
 	else if (game->map.map[y][x] == ' ')
 	{
-		check_space_bottom(game, y - 1, x);
-		check_space_bottom(game, y, x + 1);
-		check_space_bottom(game, y, x - 1);
+		check_space_bottom_left(game, y, x - 1);
+		check_space_bottom_left(game, y - 1, x);
+	}
+	else
+		ft_exit(game, 3);
+}
+
+void	check_space_bottom_right(t_game *game, int y, int x)
+{
+	if (x < 0 || y < 0 || y >= game->map.height || x >= game->map.width)
+		return ;
+	else if (game->map.map[y][x] == '1')
+		return ;
+	else if (game->map.map[y][x] == ' ')
+	{
+		check_space_bottom_right(game, y, x + 1);
+		check_space_bottom_right(game, y - 1, x);
 	}
 	else
 		ft_exit(game, 3);
@@ -100,14 +121,13 @@ void	check_closed_bottom(t_game *game, int y)
 	x = 0;
 	while (game->map.map[y][x])
 	{
-		printf("map[%d][%d] = %c\n", y, x, game->map.map[y][x]);
 		if (game->map.map[y][x] == '1')
 			;
 		else if (game->map.map[y][x] == ' ')
 		{
-			check_space_bottom(game, y - 1, x);
-			check_space_bottom(game, y - 1, x + 1);
-			check_space_bottom(game, y - 1, x - 1);
+			check_space_bottom_right(game, y - 1, x);
+			check_space_bottom_right(game, y - 1, x + 1);
+			check_space_bottom_left(game, y - 1, x - 1);
 		}
 		else
 			ft_exit(game, 3);
@@ -115,8 +135,7 @@ void	check_closed_bottom(t_game *game, int y)
 	}
 }
 
-
-void	check_space_top(t_game *game, int y, int x)
+void	check_space_top_left(t_game *game, int y, int x)
 {
 	if (x < 0 || y < 0 || y >= game->map.height || x >= game->map.width)
 		return ;
@@ -124,9 +143,23 @@ void	check_space_top(t_game *game, int y, int x)
 		return ;
 	else if (game->map.map[y][x] == ' ')
 	{
-		check_space_top(game, y + 1, x);
-		check_space_top(game, y + 1, x + 1);
-		check_space_top(game, y + 1, x - 1);
+		check_space_top_left(game, y + 1, x);
+		check_space_top_left(game, y, x - 1);
+	}
+	else
+		ft_exit(game, 3);
+}
+
+void	check_space_top_right(t_game *game, int y, int x)
+{
+	if (x < 0 || y < 0 || y >= game->map.height || x >= game->map.width)
+		return ;
+	else if (game->map.map[y][x] == '1')
+		return ;
+	else if (game->map.map[y][x] == ' ')
+	{
+		check_space_top_right(game, y + 1, x);
+		check_space_top_right(game, y, x + 1);
 	}
 	else
 		ft_exit(game, 3);
@@ -145,9 +178,9 @@ void	check_closed_top(t_game *game)
 			;
 		else if (game->map.map[y][x] == ' ')
 		{
-			check_space_top(game, y + 1, x);
-			check_space_top(game, y + 1, x + 1);
-			check_space_top(game, y + 1, x - 1);
+			check_space_top_right(game, y + 1, x);
+			check_space_top_right(game, y + 1, x + 1);
+			check_space_top_left(game, y + 1, x - 1);
 		}
 		else
 			ft_exit(game, 3);
